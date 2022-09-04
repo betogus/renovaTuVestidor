9b5020ad53237c0d5c724928d3961a0abb67e89d
Gustavo Torres <95319442+betogus@users.noreply.github.com>
2022-09-04 09:56:18 -0300

    se corrigio

--- src/components/itemListContainer/ItemListContainer.js
+++ src/components/itemListContainer/ItemListContainer.js
@@ -1,28 +1,60 @@
 import CardComponent from "./cardComponent/CardComponent";
-import { usuarios } from "../../baseDeDatos/BaseDeDatos";
+import React, {useState, useEffect} from "react";
+import {usuarios} from "../../baseDeDatos/BaseDeDatos"
 
 
+const ItemListContainer = () => {
+  const [items, setItems] = useState([]); //Creamos un array donde luego lo cargamos con info
+    useEffect(() => { // al momento de renderizar, el useEffect se dispara
+    setTimeout(()=> {
+      fetch(`${process.env.REACT_APP_API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => setItems(data))
+    },2000)
+  }, []) // Traemos de la api los users y los cargamos en el array
 
-function ItemListContainer (props) {
-    return (
+  const [initial, setInitial] = useState(0)
+  const [stock, setStock] = useState(usuarios.usuario1.clothes[0].stock)
+  const [counter, setCounter] = useState(initial)
+
+  const onAdd = (counter, stock, initial) => {
+    console.log(`Hay ${initial} productos en tu carrito. Se añadieron ${counter}. Stock: ${stock-counter}`);
+    setInitial(initial + counter);
+    setStock (stock - counter);
+    setCounter(0);
+}
+
+  return (
+    
+
         <div className="d-flex">
           <CardComponent 
-            user = {
-              {
-                logo :`${usuarios.usuario1.logo}`,
-                name : `${usuarios.usuario1.name}`,
-                clothes: [{
-                  img : `${usuarios.usuario1.clothes[0].img}`,
-                  name : `${usuarios.usuario1.clothes[0].name}`,
-                  size : `${usuarios.usuario1.clothes[0].size}`,
-                  price: `${usuarios.usuario1.clothes[0].price}`,
-                  stock: `${usuarios.usuario1.clothes[0].stock}`
-                }]
-              }
-            }
+          items = {items}
+          stock = {stock}
+          initial = {initial}
+          onAdd = {onAdd}
+          logoUsuario = {usuarios.usuario1.logo}
+          nombreUsuario = {usuarios.usuario1.name}
+          nombreRopa = {usuarios.usuario1.clothes[0].name}
+          imgRopa = {usuarios.usuario1.clothes[0].img}
+          precioRopa = {usuarios.usuario1.clothes[0].price}
+          tamanioRopa = {usuarios.usuario1.clothes[0].size}
           />
+            
       </div>
     )
 }
 
 export default ItemListContainer
+
+/* 
+en vez de usar fetch, podemos instalar axios y hacer lo siguiente:
+
+import axios from "axios"
+
+useEffect(() => { 
+    setTimeout(()=> {
+axios.get ('https://jsonplaceholder.typicode.com/photos').then(res) => console.log(res.data)
+ },2000)
+  }, []) 
+*/
--- src/components/itemListContainer/cardComponent/CardComponent.js
+++ src/components/itemListContainer/cardComponent/CardComponent.js
@@ -1,41 +1,34 @@
 import React from 'react'
 import {styles} from './CardComponentStyle';
 import ItemCount from '../itemCount/ItemCount';
-import {usuarios} from "../../../baseDeDatos/BaseDeDatos"
 
 const CardComponent = (props) => {
+
+    
+    
   return (
     <div style={styles.cardContainer}>
         <header className='d-flex align-items-end my-1'>
             <div>
-                <img src={props.user.logo} alt="usuario" style={styles.logo}></img>
+                <img src={props.logoUsuario} alt="usuario" style={styles.logo}></img>
             </div>
             <div>
-                <p className='mx-4 my-1'><b>{props.user.name}</b></p>
+                <p className='mx-4 my-1'><b>{props.nombreUsuario}</b></p>
             </div>
         </header>
         <main>
-            <img src={props.user.clothes[0].img} alt="ropa" style={styles.imgClothes}></img>
+            <img src={props.imgRopa} alt="ropa" style={styles.imgClothes}></img>
         </main>
         <footer>
             <div>
-                <p className='text-center fs-5 my-1'>{props.user.clothes[0].name}</p>
-                <p className='text-center my-1' style={{color: "orange", fontSize: "25px"}}><b>${props.user.clothes[0].price}</b></p>
-                <p className='text-center my-1'>Talle {props.user.clothes[0].size}</p>
+                <p className='text-center fs-5 my-1'>{props.nombreRopa}</p>
+                <p className='text-center my-1' style={{color: "orange", fontSize: "25px"}}><b>${props.precioRopa}</b></p>
+                <p className='text-center my-1'>Talle {props.tamanioRopa}</p>
             </div>
             <ItemCount
-                user = {
-                    { 
-                    clothes: [{
-                        stock: `${usuarios.usuario1.clothes[0].stock}`,
-                        initial: 0,
-                        onAdd: (counter, initial, stock) => {
-                                console.log(`Hay ${initial} productos en tu carrito. Se añadieron ${counter}. Stock: ${stock-counter}`);
-                            }
-                            
-                        }]
-                    }
-                }
+                        stock = {props.stock}
+                        initial = {props.initial}
+                        onAdd = {props.onAdd}
             
             />
         </footer>
